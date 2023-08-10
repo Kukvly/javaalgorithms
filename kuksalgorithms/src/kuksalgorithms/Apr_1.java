56b6e44d0c8af3e0556d97a6ec06232187161ad5
Kukvly <kukvly96@gmail.com>
2023-08-11 00:27:39 +0900

    Apr_1 needs thinking

--- kuksalgorithms/src/kuksalgorithms/Apr_1.java
+++ kuksalgorithms/src/kuksalgorithms/Apr_1.java
@@ -1,10 +1,40 @@
 package kuksalgorithms;
 
+/*
+ * 크기가 N인 집합 A={A1,A2,...AN}, 정수 K가 주어진다.
+ * 좋은 집합 조건
+ * S에는 두 개 이상의 수가 포함
+ * S의 서로 다른 두 원소 a,b(S의 원소)에 대해서 a+b는 K의 배수가 아니다.
+ * 좋은 집합의 개수는?
+ * 
+ * (2<=N, K<=100000)
+ * (1<=Ai<=10^9)
+ * */
+
+import java.util.Scanner;
+
 public class Apr_1 {
 
 	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		int n = input.nextInt();
+		int k = input.nextInt();
+		int num[] = new int[n];
+		
 		// TODO Auto-generated method stub
-		System.out.println("test");
+		System.out.println("n: " + n);
+		System.out.println("k: " + k);
+		
+		for(int i=0; i<n; i++) {
+			num[i] = input.nextInt();
+			System.out.println("num: " + num[i]);
+		}
+		
+		int count = 0;
+		
+		System.out.println("count: " + (count%1000000007));
 	} 
 
 }

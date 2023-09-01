07caa6a342051fa070a3481831ac953bdfa63a88
Kukvly <kukvly96@gmail.com>
2023-09-01 11:08:22 +0900

    base2 init

---
+++ kuksalgorithms/src/basealgorithms/base2.java
@@ -0,0 +1,26 @@
+package basealgorithms;
+
+import java.util.Scanner;
+
+public class base2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		
+		int x1 = sc.nextInt();
+		int y1 = sc.nextInt();
+		int r1 = sc.nextInt();
+		int x2 = sc.nextInt();
+		int y2 = sc.nextInt();
+		int r2 = sc.nextInt();
+		
+		if((-10000<=x1 && x1<=10000) && (-10000<=y1 && y1<=10000)
+				&& (-10000<=x2 && x2<=10000) && (-10000<=y2 && y2<=10000)
+				&& (-10000<=r1 && r1<=10000) && (-10000<=r2 && r2<=10000)){
+			
+		}
+	}
+
+}

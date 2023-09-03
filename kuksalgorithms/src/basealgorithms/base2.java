7a4d000c2bdab51baa7edba2c3e5dc3daee576b1
Kukvly <kukvly96@gmail.com>
2023-09-03 17:50:45 +0900

    un succ

--- kuksalgorithms/src/basealgorithms/base2.java
+++ kuksalgorithms/src/basealgorithms/base2.java
@@ -16,10 +16,17 @@ public class base2 {
 		int y2 = sc.nextInt();
 		int r2 = sc.nextInt();
 		
+		int rtn = 0;
+		
 		if((-10000<=x1 && x1<=10000) && (-10000<=y1 && y1<=10000)
 				&& (-10000<=x2 && x2<=10000) && (-10000<=y2 && y2<=10000)
 				&& (-10000<=r1 && r1<=10000) && (-10000<=r2 && r2<=10000)){
-			
+			if((r1+r2)== Math.sqrt(Math.pow((x1-x2), 2) + Math.pow((y1-y2), 2))) {
+				System.out.println("(x1, y1): " + "(" + x1 + ", " + y1 + ")");
+				System.out.println("(x2, y2): " + "(" + x2 + ", " + y2 + ")");
+			} else  {
+				
+			}
 		}
 	}
 
---
+++ kuksalgorithms/src/basealgorithms/base3.java
@@ -0,0 +1,52 @@
+package basealgorithms;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class base3 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+
+		int t = Integer.parseInt(s);
+		int arr[] = new int[t];
+
+		for (int i = 0; i < t; i++) {
+			int cnt0 = 0;
+			int cnt1 = 0;
+			String d = br.readLine();
+			arr[i] = Integer.parseInt(d);
+
+			int n = arr[i]; // 3
+			System.out.println("n: " + arr[i]);
+			if (n <= 40 || n == 0) {
+				if (n == 0) {
+					cnt0++;
+				} else if (n == 1) {
+					cnt1++;
+				} else {
+					fibonacci(n);
+				}
+
+			}
+			System.out.println(cnt0 + " " + cnt1);
+		}
+
+	}
+
+	public static int fibonacci(int n) {
+		if (n == 0) {
+			return 0;
+		} else if (n == 1) {
+			return 1;
+		} else {
+			return fibonacci(n - 1) + fibonacci(n - 2);
+		}
+	}
+
+}

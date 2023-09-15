cf403798e250d0ba4245a06d25ec6e54554afbeb
Kukvly <kukvly96@gmail.com>
2023-09-15 17:46:44 +0900

    succ base5

--- kuksalgorithms/src/basealgorithms/base5.java
+++ kuksalgorithms/src/basealgorithms/base5.java
@@ -1,33 +1,19 @@
 package basealgorithms;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class base5 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-//		int arr[] = new int[2];
-//		
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		String input1 = br.readLine();
-//		int a = Integer.parseInt(input1);
-//		System.out.println(a);
-//		
+	public static void main(String[] args) throws IOException {
 
-		Scanner scan = new Scanner(System.in);
-		int a = scan.nextInt();
-		int b = scan.nextInt();
-		
-		System.out.println("===============");
-		
-//		
-//		arr[0] = a;
-//		arr[1] = b;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] input = br.readLine().split(" ");
+		int a = Integer.parseInt(input[0]);
+		int b = Integer.parseInt(input[1]);
 
-		System.out.println(a);
-		System.out.println(b);
-		
-		double rtn = 0;
+		double rtn=0;
 		
 		if((a>0 && a<10)|| (b>0 && b<10)) {
 			rtn = div(a,b);

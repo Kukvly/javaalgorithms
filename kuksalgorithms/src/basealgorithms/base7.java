7a20fb65bdc7ba3b95fb912ac52b2e0a83b945de
Kukvly <kukvly96@gmail.com>
2023-09-21 10:00:21 +0900

    base7 pb init

--- kuksalgorithms/src/basealgorithms/base7.java
+++ kuksalgorithms/src/basealgorithms/base7.java
@@ -1,12 +1,20 @@
 /**
- * 
+ * 수열의 합
+ * N: 합
+ * L: 길이가 적어도 L
+ * 가장 짧은 연속된 음이 아닌 정수 리스트 출력
+ * N <= 1000000000, 2<=L<=100 [N, L 자연수]
+ * 만약 리스트의 길이가 100보다 작거나 같으면, 
+ * 연속된 수를 첫째 줄에 공백으로 구분하여 출력한다. 
+ * 만약 길이가 100보다 크거나 그러한 수열이 없을 때는 -1을 출력한다.
  */
 
 package basealgorithms;
 
 public class base7 {
 
-	
+	int n=0;
+	int l=0;
 	
 	public static void main(String[] args) {
 		

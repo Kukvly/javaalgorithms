e2a18c437d71c76dc22c78459c692f9a823e9741
Kukvly <kukvly96@gmail.com>
2023-10-06 20:05:21 +0900

    thinking

--- kuksalgorithms/src/basealgorithms/base7.java
+++ kuksalgorithms/src/basealgorithms/base7.java
@@ -19,11 +19,51 @@
  * 35 56789
  * 
  * 
+ * 
  * 홀수갯수
  * 18 5+6+7
  * ([0]+[l-1])*(l/2) = n
  * 
- *
+ * n을 쪼개서 쪼갠 값들의 중심 값을 잡아 왼쪽으로는 1씩 빼주고 오른쪽으로는 1씩 더해준다.
+ * 
+ * ex.
+ * n=35
+ * l=5
+ * nList[0] = 5
+ * nList[1] = 6
+ * nList[2] = 7
+ * nList[3] = 8
+ * nList[4] = 9
+ * 
+ * 
+ * 
+ * case1.
+ * n % 2 = 1 -> 합이 홀수 -> 중심값이 1개 있다.
+ * 중심값 = n/l
+ * nList[l].length() = l
+ * 
+ * 
+ * 중앙 인덱스: [(l-1)/2]
+ * 
+ * 
+ * 
+ *                    
+ * case2.
+ * 
+ * nList[0] = x
+ * nList[1] = x+1
+ * nList[2] = x+2
+ * nList[3] = x+3
+ * 
+ * (i+1 = l) -> 마지막 i + 1 = l (수열의 길이)
+ * 
+ * int isum = 0;
+ * isum = isum + i;
+ * n = nList[0]*(i+1) + isum
+ * 
+ * n = x+y+z
+ * 
+ * 
  * 
  */
 
@@ -45,10 +85,12 @@ public class base7 {
 
 		int sum=0;
 		
-		int nList[] = new int[n];
+		int nList[] = new int[l];
 		
 		if((l>=2 && l<=100)&& (n<=1000000000)) {
-			
+			for (int i=0; i<l; i++) {
+				nList[i] = 
+			}
 		} 
 //		else if(l>100 || 값x) {
 //			sum = -1;

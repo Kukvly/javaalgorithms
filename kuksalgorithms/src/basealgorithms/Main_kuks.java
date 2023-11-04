33347df081f1598ba7ea32ceb9a9a6b723ed071f
Kukvly <kukvly96@gmail.com>
2023-11-04 16:02:34 +0900

    base14 init

---
+++ kuksalgorithms/src/basealgorithms/Main_kuks.java
@@ -0,0 +1,12 @@
+package basealgorithms;
+
+public class Main_kuks {
+
+	public static void main(String[] args) {
+		System.out.println("\\    /"+"\\");
+		System.out.println(" )  (" +" '"+")");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|"); 
+	}
+
+}

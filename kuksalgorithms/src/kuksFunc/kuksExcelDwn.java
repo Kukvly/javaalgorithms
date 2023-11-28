c4d9492ba2ec9de76c526bda4b0fec4de786684d
Kukvly <kukvly96@gmail.com>
2023-11-28 14:33:07 +0900

    poi import success

--- kuksalgorithms/src/kuksFunc/kuksExcelDwn.java
+++ kuksalgorithms/src/kuksFunc/kuksExcelDwn.java
@@ -1,10 +1,17 @@
 package kuksFunc;
 
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.apache.poi.ss.usermodel.Workbook;
+
 public class kuksExcelDwn {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-
+		Workbook wb = null;
+		OutputStream out = null;
+		wb.write(out);
 	}
 
 }

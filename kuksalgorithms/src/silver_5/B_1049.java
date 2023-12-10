84817ea1708be4e179be5d9937527ce328945842
Kukvly <kukvly96@gmail.com>
2023-12-10 17:34:13 +0900

    1049 ing

--- kuksalgorithms/src/silver_5/B_1049.java
+++ kuksalgorithms/src/silver_5/B_1049.java
@@ -1,38 +1,45 @@
 package silver_5;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * 
  * @author kukvly
  * 
- * 사용하는 기타의 N개의 줄이 끊어졌다. 새 줄을 사거나 교체해야 한다.
- * 6줄 패키지를 살 수도 있고, 1개 또는 그 이상의 낱개 구매도 가능하다.
- * 끊어진 기타줄 개수: N
- * 기타줄 브랜드: M
- * 각각의 브랜드에서 파는 기타줄 6개가 들어있는 패키지의 가격
- * 낱개로 살 때의 가격이 주어질 때, 적어도 N개를 사기 위해 필요한 돈의 수를 최소로 하는 프로그램을 작성하시오.
+ *         사용하는 기타의 N개의 줄이 끊어졌다. 새 줄을 사거나 교체해야 한다. 6줄 패키지를 살 수도 있고, 1개 또는 그 이상의
+ *         낱개 구매도 가능하다. 끊어진 기타줄 개수: N 기타줄 브랜드: M 각각의 브랜드에서 파는 기타줄 6개가 들어있는 패키지의
+ *         가격 낱개로 살 때의 가격이 주어질 때, 적어도 N개를 사기 위해 필요한 돈의 수를 최소로 하는 프로그램을 작성하시오.
  * 
- * input
- * N M (1<=N<=100, 1<=M<=50) -> 첫째 줄
- * 둘째 줄부터 M개의 줄에는 각 브랜드의 패키지 가격과 낱개의 가격이 공백으로 구분하여 주어진다.
- * 가격은 0보다 크거나 같고, 1,000보다 작거나 같은 정수이다.
+ *         input N M (1<=N<=100, 1<=M<=50) -> 첫째 줄 둘째 줄부터 M개의 줄에는 각 브랜드의 패키지 가격과
+ *         낱개의 가격이 공백으로 구분하여 주어진다. 가격은 0보다 크거나 같고, 1,000보다 작거나 같은 정수이다.
  * 
- * output
- * 첫째 줄에 기타줄을 적어도 N개 사기 위해 필요한 돈의 최솟값 출력
+ *         output 첫째 줄에 기타줄을 적어도 N개 사기 위해 필요한 돈의 최솟값 출력
  * 
- * ex.
- * input
- * 4 2
- * 12 3
- * 15 4
+ *         ex. input 4 2 12 3 15 4
  * 
- * output
- * 12
+ *         output 12
  */
 
 public class B_1049 {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input[] = br.readLine().split(" ");
+		// n<=100
+		int n = Integer.parseInt(input[0]);
+		
+		// m<=50
+		int m = Integer.parseInt(input[1]);
+		
+		for (int i=0; i<m; i++) {
+			input = br.readLine().split(" ");
+			int tempN = Integer.parseInt(input[0]);
+			int tempM = Integer.parseInt(input[1]);
+		}
+		
+		// logic
 	}
 
 }

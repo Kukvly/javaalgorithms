9ccaf15a9d244d00948cfefe05f6d9c16f240474
Kukvly <kukvly96@gmail.com>
2023-12-18 23:57:10 +0900

    2075 init

---
+++ kuksalgorithms/src/silver_2/B_2075.java
@@ -0,0 +1,19 @@
+package silver_2;
+
+/*
+ * N x N 표
+ * 모든 수는 자신의 한 칸 위에 있는 수보다 크다.
+ * N번째 큰 수는?
+ * 1 <= N <= 1500
+ * -10억 <= 표 안에 수 <= 10
+ * 
+ * 
+ * */
+
+public class B_2075 {
+
+	public static void main(String[] args) {
+
+	}
+
+}

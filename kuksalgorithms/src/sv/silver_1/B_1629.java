4b4c3865572277d743643d09ebfbf643a369dc18
Kukvly <kukvly96@gmail.com>
2023-12-25 22:14:42 +0900

    1629 fail

--- kuksalgorithms/src/sv/silver_1/B_1629.java
+++ kuksalgorithms/src/sv/silver_1/B_1629.java
@@ -1,5 +1,9 @@
 package sv.silver_1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * 
  * @author kukvly
@@ -16,9 +20,13 @@ package sv.silver_1;
 
 public class B_1629 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input[] = br.readLine().split(" ");
+		long a = Long.parseLong(input[0]);
+		long b = Long.parseLong(input[1]);
+		long c = Long.parseLong(input[2]);
+		
+		System.out.println((long)(Math.pow(a, b)%c));
 	}
-
 }

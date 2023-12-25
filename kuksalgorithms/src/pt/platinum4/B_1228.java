90901128dddc173e73dfcda31becfebe08ffc882
Kukvly <kukvly96@gmail.com>
2023-12-25 21:43:04 +0900

    1629 init

--- kuksalgorithms/src/pt/platinum4/B_1228.java
+++ kuksalgorithms/src/pt/platinum4/B_1228.java
@@ -1,5 +1,11 @@
 package pt.platinum4;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author kukvly
  * 
@@ -34,9 +40,33 @@ package pt.platinum4;
 
 public class B_1228 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input[] = br.readLine().split(" ");
+		int n = Integer.parseInt(input[0]);
+		int m = Integer.parseInt(input[1]);
+		String country[] = new String[n];
+		
+		Map<String, Integer> pMap = new HashMap<>();
+		
+		for(int i=0; i<n; i++) {
+			input = br.readLine().split(" ");
+			country[i] = input[0];
+			System.out.println("=====================");
+			System.out.println(country[i]);
+			System.out.println("=====================");
+			pMap.put(input[0], Integer.parseInt(input[1]));
+			System.out.println(input[0] + " " + Integer.parseInt(input[1]));
+			for(int j=2; j<input.length; j++) {
+				System.out.println("input[" + j +"]: " + input[j]);
+				if(input[j] == country[i]) {
+					System.out.println(country[i]);
+				}
+			}
+		}
+		
+		
+		
 	}
 
 }
---
+++ kuksalgorithms/src/sv/silver_1/B_1629.java
@@ -0,0 +1,24 @@
+package sv.silver_1;
+
+/**
+ * 
+ * @author kukvly
+ * 자연수 A를 B번 곱한 수 구하기
+ * 단, A^B를 C로 나눈 나머지를 구하는 프로그램을 작성
+ * 
+ * A, B, C <= 2,147,483,647
+ * input
+ * A B C
+ * 
+ * output
+ * A를 B번 곱한 수를 C로 나눈 나머지 출력
+ */
+
+public class B_1629 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

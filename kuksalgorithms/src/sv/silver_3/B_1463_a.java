31e58f6d42a7e4800d42983130b4f33224cd870e
Kukvly <kukvly96@gmail.com>
2023-12-27 10:51:40 +0900

    1083 init

--- kuksalgorithms/src/sv/silver_3/B_1463_a.java
+++ kuksalgorithms/src/sv/silver_3/B_1463_a.java
@@ -15,16 +15,17 @@ public class B_1463_a {
 
 	public static int Cal(int n) {
 		int[] dp = new int[n + 1];
-
         for (int i = 2; i <= n; i++) {
             dp[i] = dp[i - 1] + 1;  // 1을 뺀 경우
-
+            
             if (i % 2 == 0) {
                 dp[i] = Math.min(dp[i], dp[i / 2] + 1);  // 2로 나눈 경우
+                System.out.println(i+": " + dp[i]+"----------2");
             }
 
             if (i % 3 == 0) {
                 dp[i] = Math.min(dp[i], dp[i / 3] + 1);  // 3으로 나눈 경우
+                System.out.println(i+": " + dp[i]+"----------3");
             }
         }
 

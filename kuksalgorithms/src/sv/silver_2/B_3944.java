8648623ef7f843749bf86b4b257974bbe69c8497
Kukvly <kukvly96@gmail.com>
2024-01-05 00:19:19 +0900

    3944 init

---
+++ kuksalgorithms/src/sv/silver_2/B_3944.java
@@ -0,0 +1,23 @@
+package sv.silver_2;
+
+/**
+ * 
+ * @author kukvly
+ * 
+ * 나머지 계산
+ * 
+ * input
+ * t: 테스트 케이스의 개수
+ * t개의 (b진법 d)
+ * 
+ * output
+ * t개의 b진법수 d를 (b-1)로 나눈 나머지 출력 
+ */
+
+public class B_3944 {
+
+	public static void main(String[] args) {
+		
+	}
+
+}

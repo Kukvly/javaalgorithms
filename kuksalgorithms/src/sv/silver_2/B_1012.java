c44080c5197929a04903a45a895c8f9c4b3e2280
Kukvly <kukvly96@gmail.com>
2024-01-12 12:52:49 +0900

    1012 ing

--- kuksalgorithms/src/sv/silver_2/B_1012.java
+++ kuksalgorithms/src/sv/silver_2/B_1012.java
@@ -1,10 +1,15 @@
 package sv.silver_2;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * 
  * @author kukvly
  * 유기농 배추
  * 한 배추의 상하좌우 네 방향에 다른 배추가 위치한 경우 -> 서로 인접해있는 것
+ * T: 테스트 케이스 수
  * M: 가로
  * N: 세로
  * (1<=M,N<=50)
@@ -16,9 +21,26 @@ package sv.silver_2;
 
 public class B_1012 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		String input[] = br.readLine().split(" ");
+		
+		for(int i=0;i<t;i++) {
+			int m = Integer.parseInt(input[0]);
+			int n = Integer.parseInt(input[1]);
+			int k = Integer.parseInt(input[2]);
+			
+			int vegArr[][] = new int[m][n];
+			
+			for (int j=0; j<k; j++) {
+				input = br.readLine().split(" ");
+				int x = Integer.parseInt(input[0]);
+				int y = Integer.parseInt(input[1]);
+				vegArr[x][y] = 1;
+			}
+		}
+		
 	}
 
 }

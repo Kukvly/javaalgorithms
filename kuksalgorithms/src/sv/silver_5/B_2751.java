267b78f9672f003dffb4c7ea4dc001d4f8072862
Kukvly <kukvly96@gmail.com>
2024-01-15 23:47:53 +0900

    2751 init

---
+++ kuksalgorithms/src/sv/silver_5/B_2751.java
@@ -0,0 +1,19 @@
+package sv.silver_5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B_2751 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int numArr[] = new int[n];
+		for (int i=0; i<n; i++) {
+			numArr[i] = Integer.parseInt(br.readLine());
+		}
+		
+	}
+
+}

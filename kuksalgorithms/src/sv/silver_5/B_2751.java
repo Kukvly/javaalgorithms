8c7aa3d035745e880269a0d4aa2607cc06495a33
Kukvly <kukvly96@gmail.com>
2024-01-15 23:52:53 +0900

    2751 timeover

--- kuksalgorithms/src/sv/silver_5/B_2751.java
+++ kuksalgorithms/src/sv/silver_5/B_2751.java
@@ -3,17 +3,22 @@ package sv.silver_5;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class B_2751 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		int numArr[] = new int[n];
+		List<Integer> numList = new ArrayList<Integer>();
 		for (int i=0; i<n; i++) {
-			numArr[i] = Integer.parseInt(br.readLine());
+			numList.add(Integer.parseInt(br.readLine()));
 		}
-		
+		Collections.sort(numList);
+		for(int i=0; i<n; i++) {
+			System.out.println(numList.get(i));
+		}
 	}
-
 }

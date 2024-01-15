efae43b8499723ac3f986037435071af6c5c1015
Kukvly <kukvly96@gmail.com>
2024-01-16 00:14:35 +0900

    2751 clear

--- kuksalgorithms/src/sv/silver_5/B_2751.java
+++ kuksalgorithms/src/sv/silver_5/B_2751.java
@@ -1,24 +1,31 @@
 package sv.silver_5;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
 
 public class B_2751 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int n = Integer.parseInt(br.readLine());
-		List<Integer> numList = new ArrayList<Integer>();
+		int numArr[] = new int[n];
+				
 		for (int i=0; i<n; i++) {
-			numList.add(Integer.parseInt(br.readLine()));
+			numArr[i]=(Integer.parseInt(br.readLine()));
 		}
-		Collections.sort(numList);
-		for(int i=0; i<n; i++) {
-			System.out.println(numList.get(i));
+		
+		Arrays.sort(numArr);
+		
+		for (int i=0; i<n; i++) {
+			bw.write(numArr[i]+"\n");
 		}
+		
+		bw.flush();
+		bw.close();
 	}
 }

c59f8876b690bd6e4770865dd770abe634854e8d
Kukvly <kukvly96@gmail.com>
2024-01-19 00:04:00 +0900

    1312 clear

--- kuksalgorithms/src/sv/silver_5/B_1312.java
+++ kuksalgorithms/src/sv/silver_5/B_1312.java
@@ -15,13 +15,24 @@ import java.io.InputStreamReader;
 
 public class B_1312 {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String input[] = br.readLine().split(" ");
-		int a = Integer.parseInt(input[0]);
-		int b = Integer.parseInt(input[1]);
-		int n = Integer.parseInt(input[2]);
-		
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input[] = br.readLine().split(" ");
+        int a = Integer.parseInt(input[0]);
+        int b = Integer.parseInt(input[1]);
+        int n = Integer.parseInt(input[2]);
 
+        if (a % b == 0) {
+            System.out.println(0);
+        } else {
+            a %= b;
+            int result = 0;
+            for (int i = 0; i < n; i++) {
+                a *= 10;
+                result = a / b;
+                a %= b;
+            }
+            System.out.println(result);
+        }
+    }
 }

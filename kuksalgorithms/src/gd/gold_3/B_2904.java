2d963777213c6afe8631118c0ff6e71f964f027d
Kukvly <kukvly96@gmail.com>
2024-01-22 12:47:33 +0900

    2904 init

---
+++ kuksalgorithms/src/gd/gold_3/B_2904.java
@@ -0,0 +1,22 @@
+package gd.gold_3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 
+ * @author kukvly
+ * a
+ *
+ */
+
+public class B_2904 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		String num[] = br.readLine().split(" ");
+		
+	}
+}

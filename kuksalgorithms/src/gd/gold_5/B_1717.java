424f7729448ea12a6dd154e5e63fc28d516236bd
Kukvly <kukvly96@gmail.com>
2024-01-25 17:37:32 +0900

    1717 50%

--- kuksalgorithms/src/gd/gold_5/B_1717.java
+++ kuksalgorithms/src/gd/gold_5/B_1717.java
@@ -3,7 +3,9 @@ package gd.gold_5;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
@@ -28,13 +30,14 @@ import java.util.Random;
 
 public class B_1717 {
 
+	@SuppressWarnings("rawtypes")
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String input[] = br.readLine().split(" ");
 		int n = Integer.parseInt(input[0]);
 		int m = Integer.parseInt(input[1]);	// m: 연산 개수
 		
-		int numArr[] = new int[n+1];
+		
 
 		Random rd = new Random();
 		int rdIdx = rd.nextInt(2);
@@ -45,29 +48,93 @@ public class B_1717 {
 		
 		
 		Map<Integer, Integer> numMap = new HashMap<Integer, Integer>();
+		List<Map> numList = new ArrayList<Map>();
+		/**
+		 * 0 1 2
+		 * 0 3 2
+		 * 
+		 *  0 1 2 를 하면
+		 *  {1, 2}가 되게끔
+		 *  그리고
+		 *  {1, 2}에 3만 있는지, 2만 있는지, 3과 2 둘 다 있는지 체크
+		 *  그 후 
+		 *  3만 있으면 2를 추가 (input[1]만 있으면 input[2]만 추가)
+		 *  2만 있으면 3을 추가 (input[2]만 있으면 input[1]만 추가)
+		 *  둘 다 있으면 pass
+		 *  else -> 둘 다 없으면
+		 *  {2, 3} 새로 만들어준다.
+		 *  
+		 */
+		int numArr[] = new int[n+1];
 		
-		for (int i=0; i<n+1; i++) {
+		for(int i=0; i<n+1; i++) {
+			numArr[i]=i;
+		}
+		
+		input = br.readLine().split(" ");
+		
+		numMap.put(Integer.parseInt(input[1]),Integer.parseInt(input[1]));
+		numMap.put(Integer.parseInt(input[2]),Integer.parseInt(input[2]));
+		
+		numList.add(numMap);
+
+		for (int i=1; i<m+1; i++) {
+			if (i==m) {
+				return;
+			}
+			numMap.clear();
 			input = br.readLine().split(" ");
-			numArr[i] = i;
+			int a = Integer.parseInt(input[1]);
+			int b = Integer.parseInt(input[2]);
+			
+//			System.out.println("----------------------");
+//			System.out.println("a: " + a);
+//			System.out.println("b: " + b);
+//			System.out.println("----------------------");
+			
+			
+//			System.out.println("numMap.toString(): " + numMap.toString());
+			
+			
 			if(input[0].equals("0")) {
-				
+				for(int j=0; j<numList.size(); j++) {
+					if(numList.get(i-1).containsValue(numMap.get(a))
+							&& !numList.get(i-1).containsValue(numMap.get(b))) {
+						numList.get(j).put(b, b);
+					} else if(!numList.get(i-1).containsValue(numMap.get(a))
+							&& numList.get(i-1).containsValue(numMap.get(b))) {
+						numList.get(j).put(a, a);
+					} else if (numList.get(i-1).containsValue(numMap.get(a))
+							&& numList.get(i-1).containsValue(numMap.get(b))) {
+						
+					} else {
+						numMap.put(a, a);
+						numMap.put(b, b);
+					}
+					numList.add(numMap);
+				}
 			} else if(input[0].equals("1")) {
-				int a = Integer.parseInt(input[1]);
-				int b = Integer.parseInt(input[2]);
-				
-				if(elmContain(numMap,a,b)) {
+				if(elmContain(numList.get(i-1),a,b)) {
 					System.out.println(sayYes[rdIdx]);
 				} else {
 					System.out.println(sayNo[rdIdx]);
 				}
 			}
+			
+		}
+		for(int k=0; k<numList.size(); k++) {
+			System.out.println(numList.get(k).toString());
 		}
 	}
 	
 	public static Boolean elmContain(Map<Integer, Integer> kuksMap, int a, int b) {
+		System.out.println("kuksmap access");
 		if(kuksMap.containsValue(a) && kuksMap.containsValue(b)) {
+			System.out.println("yun");
 			return true;
-		} else return false;
+		} else 
+			System.out.println("kuk");
+			return false;
 	}
 	
 	

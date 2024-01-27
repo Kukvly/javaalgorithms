7e0f4cb92b701d4be2d985e948bb3a229a39f2ad
Kukvly <kukvly96@gmail.com>
2024-01-27 21:18:21 +0900

    1041 init

---
+++ kuksalgorithms/src/gd/gold_5/B_1041.java
@@ -0,0 +1,30 @@
+package gd.gold_5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 
+ * @author kukvly
+ * n
+ * a b c d e f
+ *
+ */
+
+public class B_1041 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		String dice[] = br.readLine().split(" ");
+		int a = Integer.parseInt(dice[0]);
+		int b = Integer.parseInt(dice[1]);
+		int c = Integer.parseInt(dice[2]);
+		int d = Integer.parseInt(dice[3]);
+		int e = Integer.parseInt(dice[4]);
+		int f = Integer.parseInt(dice[5]);
+		
+	}
+
+}

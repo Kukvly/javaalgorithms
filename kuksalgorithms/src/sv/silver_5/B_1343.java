8f96795b6ff5c39069d2bd095f458aad57bb20b6
Kukvly <kukvly96@gmail.com>
2024-01-30 10:49:08 +0900

    1343 init

---
+++ kuksalgorithms/src/sv/silver_5/B_1343.java
@@ -0,0 +1,20 @@
+package sv.silver_5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B_1343 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String patternA = "AAAA";
+		String patternB = "BB";
+		
+		String input = br.readLine();
+		if(input.contains("..")) {
+			
+		}
+	}
+
+}

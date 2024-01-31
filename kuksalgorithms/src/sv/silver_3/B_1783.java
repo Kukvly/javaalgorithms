f1d7adea24610059b2123125b034e9ec9f91e56f
Kukvly <kukvly96@gmail.com>
2024-01-31 16:28:49 +0900

    1783 init

---
+++ kuksalgorithms/src/sv/silver_3/B_1783.java
@@ -0,0 +1,22 @@
+package sv.silver_3;
+
+/**
+ * 
+ * @author kukvly
+ * N x M
+ * 병든 나이트 위치 -> 체스판 가장 왼쪽아래 칸
+ * 
+ * 1. up 2 & right 1
+ * 2. up 1 & right 2
+ * 3. down 1 & right 2
+ * 4. down 2 & right 1
+ *
+ */
+
+public class B_1783 {
+
+	public static void main(String[] args) {
+		
+	}
+
+}

f4f8b40a0e3eb07d7ec4876045b98be305d0a2fb
Kukvly <kukvly96@gmail.com>
2024-02-05 16:00:40 +0900

    1003 solve

--- kuks.test.practice/src/kuks/test/practice/dp/B_1003_fibonacci.java
+++ kuks.test.practice/src/kuks/test/practice/dp/B_1003_fibonacci.java
@@ -4,38 +4,32 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/**
- * 
- * @author kukvly
- *
- */
-
 public class B_1003_fibonacci {
-	public static int zeroCnt, oneCnt = 0;
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int t = Integer.parseInt(br.readLine());
-		
-		for (int i=0; i<t; i++) {
-			zeroCnt = 0;
-			oneCnt = 0;
-			int n = Integer.parseInt(br.readLine());
-			fibonacci(n);
-			System.out.println(zeroCnt + " " + oneCnt);
-			
-		}
-	}
-	
-	public static int fibonacci(int n) {
-		if(n==0) {
-			zeroCnt++;
-			return 0;
-		} else if(n==1) {
-			oneCnt++;
-			return 1;
-		} else {
-			return fibonacci(n-1) + fibonacci(n-2);
-		}
-	}
-		
+    public static int[] zeroCnt, oneCnt;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        zeroCnt = new int[41];
+        oneCnt = new int[41];
+
+        for (int i = 0; i < t; i++) {
+            int n = Integer.parseInt(br.readLine());
+            fibonacci(n);
+            System.out.println(zeroCnt[n] + " " + oneCnt[n]);
+        }
+    }
+
+    public static void fibonacci(int n) {
+        zeroCnt[0] = 1;
+        oneCnt[0] = 0;
+        zeroCnt[1] = 0;
+        oneCnt[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            zeroCnt[i] = zeroCnt[i - 1] + zeroCnt[i - 2];
+            oneCnt[i] = oneCnt[i - 1] + oneCnt[i - 2];
+        }
+    }
 }

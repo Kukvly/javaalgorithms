0f7c33b0556b172aba99bdbd585a40ae090ed61e
Kukvly <kukvly96@gmail.com>
2024-03-29 21:16:30 +0900

    9184 failed

--- kuksalgorithms/src/sv/silver_2/B_9184.java
+++ kuksalgorithms/src/sv/silver_2/B_9184.java
@@ -6,6 +6,9 @@ import java.io.InputStreamReader;
 
 public class B_9184 {
 
+	// add
+	static int[][][] dp = new int[21][21][21];
+	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
@@ -26,12 +29,16 @@ public class B_9184 {
 	public static int w(int a, int b, int c) {
 		if (a<=0 || b<=0 || c<=0) {
 			return 1;
-		} else if(a>20 || b>20 || c>20) {
+		}
+		if(a>20 || b>20 || c>20) {
 			return w(20, 20, 20);
-		} else if(a<b && b<c) {
-			return (w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c));
+		} 
+		if(dp[a][b][c] != 0) return dp[a][b][c];
+		
+		if(a<b && b<c) {
+			return dp[a][b][c] = (w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c));
 		} else {
-			return w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1);
+			return dp[a][b][c] = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1);
 		}
 	}
 

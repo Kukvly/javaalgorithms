6eb19f86f49b3523f89f96d3db89f9ea2cf3c37f
Kukvly <kukvly96@gmail.com>
2024-04-30 13:51:08 +0900

    1904 mistake

---
+++ kuksalgorithms/src/sv/silver_3/B_1904.java
@@ -0,0 +1,24 @@
+package sv.silver_3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B_1904 {
+    public static int dp[] = new int[1000001];
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for(int i=3; i<=n; i++){
+            dp[i] = (dp[i-1] + dp[i-2])% 15746;    
+        }
+		
+        System.out.println(dp[n]);
+		
+	}
+
+}
--- kuksalgorithms/src/sv/silver_5/B_9656.java
+++ kuksalgorithms/src/sv/silver_5/B_9656.java
@@ -12,10 +12,10 @@ public class B_9656 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		if(n%3==2) {
-			System.out.println("CY");
-		} else {
+		if(n%2==0) {
 			System.out.println("SK");
+		} else {
+			System.out.println("CY");
 		}
 	}
 }

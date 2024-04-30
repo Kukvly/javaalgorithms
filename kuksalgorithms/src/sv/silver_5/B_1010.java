4b365ca24e9300967b35e8b4a0a32b03f1c1eab3
Kukvly <kukvly96@gmail.com>
2024-04-30 09:15:39 +0900

    1010 init

---
+++ kuksalgorithms/src/sv/silver_5/B_1010.java
@@ -0,0 +1,24 @@
+package sv.silver_5;
+
+/**
+ * (0<n<=m<30)
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B_1010 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		for(int i=0; i<t; i++) {
+			String input[] = br.readLine().split(" ");
+			int n = Integer.parseInt(input[0]);
+			int m = Integer.parseInt(input[1]);
+			
+		}
+	}
+
+}

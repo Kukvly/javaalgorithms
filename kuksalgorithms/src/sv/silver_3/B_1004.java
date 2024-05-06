fb6f373e2c89a41cb421b89e8e632bbf94d0813c
Kukvly <kukvly96@gmail.com>
2024-05-07 08:41:56 +0900

    1004 init

---
+++ kuksalgorithms/src/sv/silver_3/B_1004.java
@@ -0,0 +1,22 @@
+package sv.silver_3;
+
+/**
+ * input
+ * 1. 테스트케이스
+ * 2. 출발점 도착점
+ * 3. 행성계 개수
+ * 4. 행성계 중점 & 반지름
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B_1004 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+	}
+
+}

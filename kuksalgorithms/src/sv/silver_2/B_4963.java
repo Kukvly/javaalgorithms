d0b09751f096e79cefa073a55a39afe02eae3c57
Kukvly <kukvly96@gmail.com>
2024-05-09 14:18:29 +0900

    4963 understood

--- kuksalgorithms/src/sv/silver_2/B_4963.java
+++ kuksalgorithms/src/sv/silver_2/B_4963.java
@@ -14,48 +14,70 @@ import java.io.InputStreamReader;
 
 public class B_4963 {
 	
-	static int w,h;
+	static int w,h,cnt;
 	
 	static int arr[][];
 	
-	static int[] dx = {1, 0, -1, 0};
-	static int[] dy = {1, 0, -1, 0};
+	static boolean[][] visited;
+	
+	
+	// 8방
+	static int[] dx = {1, -1, 0, 0, 1, 1, -1, -1};
+	static int[] dy = {0, 0, 1, -1, 1, -1, 1, -1};
 	
 	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
 	
-		String input[] = br.readLine().split(" ");
 
-		w = Integer.parseInt(input[0]);
-		h = Integer.parseInt(input[1]);
-		
-		while(w!=0 && h!=0) {
-			arr = new int[w][h];
-			for (int i=0; i<w; i++) {
+		while(true) {
+			String input[] = br.readLine().split(" ");
+
+			w = Integer.parseInt(input[0]);
+			h = Integer.parseInt(input[1]);
+						
+			if(w==0 && h==0) break;
+			
+			arr = new int[h][w];
+			visited = new boolean[h][w];
+
+			for (int i=0; i<h; i++) {
 				input = br.readLine().split(" ");
-				for(int j=0; j<h; j++) {
+				for(int j=0; j<w; j++) {
 					arr[i][j] = Integer.parseInt(input[j]);
 				}
 			}
 			
 			// logic 구현부
+			cnt = 0;
+			for(int i=0; i<h; i++) {
+				for(int j=0; j<w; j++) {
+					if (arr[i][j] == 1 && visited[i][j]==false) {
+                        cnt++;
+                        dfs(i, j);
+                    } 
+				}
+			}
 			
-			dfs();
-			
+			System.out.println(cnt);
 			//
 			
-			input = br.readLine().split(" ");
-			w = Integer.parseInt(input[0]);
-			h = Integer.parseInt(input[1]);
-				
 		}
 		
 		
 	}
 	
-	public static void dfs(int start) {
+	public static void dfs(int x, int y) {
+		visited[x][y] = true;
 		
+		for(int i=0; i<8; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			
+			if(nx>=0 && ny>=0 && nx<h && ny<w && arr[nx][ny]==1 && visited[nx][ny]==false) {
+				dfs(nx, ny);
+			}
+		}
 	}
 
 }
